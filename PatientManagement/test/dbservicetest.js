9ae1f4f5d2cbd545ad3abffe9938346e6ffc5945
q <q>
2017-08-18 14:59:12 +0530

    FIrst Test

---
+++ PatientManagement/test/dbservicetest.js
@@ -0,0 +1,13 @@
+﻿describe('.readPatient()', function () {
+    it('should exist', function () {
+        expect(DBService.readPatient).toBeDefined();
+    });
+
+    it('should return one patient object if it exists', function () {
+        expect(DBService.readPatient('101')).toEqual(singlePatient);
+    });
+
+    it('should return undefined if the patient cannot be found', function () {
+        expect(DBService.readPatient('108')).not.toBeDefined();
+    });
+})
